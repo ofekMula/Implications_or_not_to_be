1e51d8241672d50ca0f8481a52ef77c14f722839
Ofek Mula <ofek1210@gmail.com>
2020-08-05 11:12:03 +0300

    add icon

--- Authentication.cs
+++ Authentication.cs
@@ -278,6 +278,7 @@ namespace AlertsProject
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "Authentication";
--- Implications.cs
+++ Implications.cs
@@ -909,6 +909,7 @@ namespace AlertsProject
             this.Controls.Add(this.label3);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "Implications";
